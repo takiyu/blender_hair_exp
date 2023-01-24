aa35e2b4b95442cfe4874a29408dcabf48951784
Hans Goudey <h.goudey@me.com>
2023-01-23 18:04:30 -0600

    Cleanup

--- source/blender/blenkernel/intern/subdiv_ccg_material.cc
+++ source/blender/blenkernel/intern/subdiv_ccg_material.cc
@@ -46,7 +46,6 @@ void BKE_subdiv_ccg_material_flags_init_from_mesh(
       &mesh->pdata, CD_PROP_INT32, "material_index");
   data->sharp_faces = (const bool *)CustomData_get_layer_named(
       &mesh->pdata, CD_PROP_BOOL, "sharp_face");
-
   material_flags_evaluator->eval_material_flags = subdiv_ccg_material_flags_eval;
   material_flags_evaluator->free = subdiv_ccg_material_flags_free;
   material_flags_evaluator->user_data = data;
