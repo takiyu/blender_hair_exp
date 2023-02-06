9673deb3072bdbfc7eb8ce7ec2c4407364617cbb
Miguel Pozo <pragma37@gmail.com>
2023-02-06 19:57:35 +0100

    EEVEE Shadows: Fix transparent bbox size
    Box vertices goes from -1 to +1

--- source/blender/draw/engines/eevee_next/shaders/eevee_shadow_tag_usage_vert.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_shadow_tag_usage_vert.glsl
@@ -13,9 +13,9 @@ void main()
   ObjectBounds bounds = bounds_buf[drw_ResourceID];
 
   interp.P = bounds.bounding_corners[0].xyz;
-  interp.P += bounds.bounding_corners[1].xyz * pos.x;
-  interp.P += bounds.bounding_corners[2].xyz * pos.y;
-  interp.P += bounds.bounding_corners[3].xyz * pos.z;
+  interp.P += bounds.bounding_corners[1].xyz * max(0, pos.x);
+  interp.P += bounds.bounding_corners[2].xyz * max(0, pos.y);
+  interp.P += bounds.bounding_corners[3].xyz * max(0, pos.z);
   interp.vP = point_world_to_view(interp.P);
 
   gl_Position = point_world_to_ndc(interp.P);
